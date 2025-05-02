11408e75b1cc7ee2ffb3cb6d89da6a72307ef4fb
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-02 14:14:13 -0300

    feat: implements constructors, getters and setters and equals/hashCode

--- api/workgym/src/main/java/ais/io/workgym/entities/SocialMedia.java
+++ api/workgym/src/main/java/ais/io/workgym/entities/SocialMedia.java
@@ -2,6 +2,7 @@ package ais.io.workgym.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -13,4 +14,53 @@ public class SocialMedia {
     private UUID uuid;
     private String name;
     private String link;
+
+    public SocialMedia() {
+    }
+
+    public SocialMedia(UUID uuid, String name, String link) {
+        this.uuid = uuid;
+        this.name = name;
+        this.link = link;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SocialMedia that = (SocialMedia) o;
+        return Objects.equals(uuid, that.uuid) && Objects.equals(name, that.name) && Objects.equals(link, that.link);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(uuid);
+        result = 31 * result + Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(link);
+        return result;
+    }
 }
