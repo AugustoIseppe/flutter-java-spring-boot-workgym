9a9ea462a4f05ca5e977e0c2efb04a3c7f8116f1
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-02 10:52:24 -0300

    first commit

---
+++ api/workgym/src/test/java/ais/io/workgym/WorkgymApplicationTests.java
@@ -0,0 +1,13 @@
+package ais.io.workgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
