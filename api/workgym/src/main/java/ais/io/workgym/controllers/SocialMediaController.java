f1898fa063b6bdbf8d9ef4e367b2e3282d95d42d
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-02 15:06:35 -0300

    feat: implemented insert social media method

--- api/workgym/src/main/java/ais/io/workgym/controllers/SocialMediaController.java
+++ api/workgym/src/main/java/ais/io/workgym/controllers/SocialMediaController.java
@@ -1,4 +1,30 @@
 package ais.io.workgym.controllers;
 
+import ais.io.workgym.dto.SocialMediaDTO;
+import ais.io.workgym.services.SocialMediaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/social-media")
 public class SocialMediaController {
+
+    @Autowired
+    private SocialMediaService socialMediaService;
+
+    @PostMapping
+    public ResponseEntity<SocialMediaDTO> insert(@Valid @RequestBody SocialMediaDTO socialMediaDTO) {
+        socialMediaDTO = socialMediaService.insert(socialMediaDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(socialMediaDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(socialMediaDTO);
+    }
+
 }
