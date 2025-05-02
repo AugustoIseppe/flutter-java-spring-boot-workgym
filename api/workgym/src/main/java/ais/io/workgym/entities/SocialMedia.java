83659e866c7c16f465ab073be9376d22ee641a24
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-02 11:21:31 -0300

    feat: implement entity socialmedia

---
+++ api/workgym/src/main/java/ais/io/workgym/entities/SocialMedia.java
@@ -0,0 +1,17 @@
+package ais.io.workgym.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_social_media")
+public class SocialMedia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+    private String name;
+    private String link;
+
+}
