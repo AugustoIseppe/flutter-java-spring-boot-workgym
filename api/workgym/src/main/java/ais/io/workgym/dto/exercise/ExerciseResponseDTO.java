1994a4ea279c866b0e1aacad181d9d9fa8d78f1f
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-05 08:37:22 -0300

    refactor: removed setters.

--- api/workgym/src/main/java/ais/io/workgym/dto/exercise/ExerciseResponseDTO.java
+++ api/workgym/src/main/java/ais/io/workgym/dto/exercise/ExerciseResponseDTO.java
@@ -34,52 +34,27 @@ public class ExerciseResponseDTO {
         image = exerciseEntity.getImage();
     }
 
-
     public UUID getId() {
         return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getMuscleGroup() {
         return muscleGroup;
     }
 
-    public void setMuscleGroup(String muscleGroup) {
-        this.muscleGroup = muscleGroup;
-    }
-
     public String getEquipment() {
         return equipment;
     }
 
-    public void setEquipment(String equipment) {
-        this.equipment = equipment;
-    }
-
     public String getImage() {
         return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
     }
 }
