f74e8898e34d8c9c36ebc30f4c418257ce530ed1
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-07 14:50:49 -0300

    feat: inserted loader and consumer

--- mobile/workgym/lib/app/modules/auth/login/login_page.dart
+++ mobile/workgym/lib/app/modules/auth/login/login_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 // import 'package:provider/provider.dart';
 import 'package:validatorless/validatorless.dart';
 import 'package:workgym/app/modules/auth/login/login_controller.dart';
+import 'package:workgym/app/widgets/loader.dart';
 import 'package:workgym/app/widgets/todo_list_field.dart';
 
 class LoginPage extends StatefulWidget {
@@ -22,61 +23,94 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TodoListField(
-                  focusNode: _emailFocus,
-                  label: 'Login',
-                  controller: _emailEC,
-                  // validator: Validatorless.multiple([
-                  //   Validatorless.required('E=mail obrigatório'),
-                  //   Validatorless.email('Email inválido'),
-                  // ]),
-                ),
-                const SizedBox(height: 10),
-                TodoListField(
-                  label: 'Senha',
-                  obscureText: true,
-                  controller: _passwordEC,
-                  validator: Validatorless.multiple([
-                    Validatorless.required('Senha obrigatória'),
-                    Validatorless.min(
-                      6,
-                      'A senha deve ter no mínimo 6 caracteres',
-                    ),
-                  ]),
-                ),
-                TextButton(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(
-                      Colors.blueAccent.shade700,
-                    ),
+        child: Consumer<LoginController>(
+          builder: (context, controller, child) {
+          return Stack(
+            children: [
+           Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TodoListField(
+                    focusNode: _emailFocus,
+                    label: 'Login',
+                    controller: _emailEC,
+                    // validator: Validatorless.multiple([
+                    //   Validatorless.required('E=mail obrigatório'),
+                    //   Validatorless.email('Email inválido'),
+                    // ]),
                   ),
-                  onPressed: () {
-                    final formValid = _formKey.currentState!.validate();
-                    if (formValid) {
-                      final email = _emailEC.text;
-                      final password = _passwordEC.text;
-                      context.read<LoginController>().login(email, password);
-                    }
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  const SizedBox(height: 10),
+                  TodoListField(
+                    label: 'Senha',
+                    obscureText: true,
+                    controller: _passwordEC,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Senha obrigatória'),
+                      Validatorless.min(
+                        6,
+                        'A senha deve ter no mínimo 6 caracteres',
+                      ),
+                    ]),
                   ),
-                ),
-              ],
+                  Consumer<LoginController>(
+                    builder: (context, controller, child) {
+                      return controller.isLoading
+                          ? const Center(child: CircularProgressIndicator())
+                          : TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: WidgetStateProperty.all(
+                                Colors.blueAccent.shade700,
+                              ),
+                            ),
+                            onPressed: () async {
+                              final formValid = _formKey.currentState!.validate();
+                              if (formValid) {
+                                final email = _emailEC.text;
+                                final password = _passwordEC.text;
+                                final token = await context
+                                    .read<LoginController>()
+                                    .login(email, password);
+          
+                                if (token.isNotEmpty) {
+                                  Navigator.of(
+                                    context,
+                                  ).pushReplacementNamed('/home-page');
+                                } else {
+                                  final error =
+                                      context.read<LoginController>().error;
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                        error ?? 'Erro ao fazer login',
+                                      ),
+                                    ),
+                                  );
+                                }
+                              }
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
+          Loader(show: controller.isLoading),
+          ],
+          );
+          },
         ),
       ),
     );
