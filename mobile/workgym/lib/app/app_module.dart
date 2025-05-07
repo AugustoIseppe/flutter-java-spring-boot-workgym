9e48e4b8b6945f307c8569b8df423efaeab8e1e2
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-07 11:10:37 -0300

    feat: add user providers

--- mobile/workgym/lib/app/app_module.dart
+++ mobile/workgym/lib/app/app_module.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:workgym/app/screens/app_widget.dart';
+import 'package:workgym/app/app_widget.dart';
+import 'package:workgym/app/modules/auth/login/login_controller.dart';
+import 'package:workgym/app/repositories/user_repository.dart';
+import 'package:workgym/app/repositories/user_repository_impl.dart';
+import 'package:workgym/app/services/user_service.dart';
+import 'package:workgym/app/services/user_service_impl.dart';
 
 class AppModule extends StatelessWidget {
+  const AppModule({super.key});
 
-  const AppModule({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return MultiProvider(
-         providers: [
-          Provider(create: (_) => Object()),
-         ],
-         child: MaterialApp(
-           title: 'WorkGym',
-           theme: ThemeData(
-             primarySwatch: Colors.blue,
-           ),
-           home: const AppWidget(), // Replace with your actual home page widget
-         ),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<UserRepository>(
+          create: (_) => UserRepositoryImpl(),
+        ),
+        Provider<UserService>(
+          create: (context) => UserServiceImpl(
+            userRepository: context.read<UserRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider<LoginController>(
+          create: (context) => LoginController(
+            userService: context.read<UserService>(),
+          ),
+        ),
+      ],
+      child: const AppWidget(),
+    );
   }
 }
