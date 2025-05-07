7c579e8a90d413b66f82401ba0babf057bcdbfd5
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-07 14:51:51 -0300

    refactor: changed return to string type

--- mobile/workgym/lib/app/repositories/user_repository.dart
+++ mobile/workgym/lib/app/repositories/user_repository.dart
@@ -1,7 +1,6 @@
-import 'package:workgym/app/dto/login_response_dto.dart';
 
 abstract class UserRepository {
-  Future<LoginResponseDto?> login(String login, String password);
+  Future<String> login(String login, String password);
   Future<Map<String, dynamic>?> tryAutoLogin(String token);
   Future<Map<String, dynamic>> getMe(String token);
 }
