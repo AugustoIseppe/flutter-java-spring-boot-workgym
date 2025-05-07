5f6cc364e0b812039783f2a2abbbd700979ecb7b
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-07 16:06:50 -0300

    feat: update loader

--- mobile/workgym/lib/app/widgets/loader.dart
+++ mobile/workgym/lib/app/widgets/loader.dart
@@ -2,26 +2,39 @@ import 'package:flutter/material.dart';
 
 class Loader extends StatelessWidget {
   final bool show;
+  final String? message;
 
-  const Loader({super.key, required this.show});
+  const Loader({super.key, required this.show, this.message});
 
   @override
   Widget build(BuildContext context) {
-    if (!show) return const SizedBox.shrink();
-
-    return Positioned.fill(
-      child: Container(
-        color: Colors.black.withValues(alpha: .5),
-        child: const Center(
-          child: Text('Loading...',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      child: show
+          ? Container(
+              color: Colors.black.withValues(alpha: .5),
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                  if (message != null) ...[
+                    const SizedBox(height: 20),
+                    Text(
+                      message!,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ],
+              ),
+            )
+          : const SizedBox.shrink(),
     );
   }
 }
