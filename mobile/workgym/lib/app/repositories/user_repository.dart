8f8b1618539ce5c17da3cc0c8fab68b33c2e4193
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-07 12:10:23 -0300

    feat: getMe method

--- mobile/workgym/lib/app/repositories/user_repository.dart
+++ mobile/workgym/lib/app/repositories/user_repository.dart
@@ -3,4 +3,5 @@ import 'package:workgym/app/dto/login_response_dto.dart';
 abstract class UserRepository {
   Future<LoginResponseDto?> login(String login, String password);
   Future<Map<String, dynamic>?> tryAutoLogin(String token);
+  Future<Map<String, dynamic>> getMe(String token);
 }
