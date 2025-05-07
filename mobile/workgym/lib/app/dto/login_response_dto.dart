40bd821b4880a9f00d49c3a417d012a1a6d93e0c
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-07 11:14:21 -0300

    feat: login response dto

---
+++ mobile/workgym/lib/app/dto/login_response_dto.dart
@@ -0,0 +1,13 @@
+class LoginResponseDto {
+  final String? accessToken;
+
+  LoginResponseDto({this.accessToken});
+
+  factory LoginResponseDto.fromJson(Map<String, dynamic> json) {
+    return LoginResponseDto(accessToken: json['accessToken']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'accessToken': accessToken};
+  }
+}
