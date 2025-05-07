71ded60bbd61c0d0813c8a43d88a5924e2db294e
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-07 12:08:54 -0300

    refactor: remove password and acccessToken

--- mobile/workgym/lib/app/models/user_model.dart
+++ mobile/workgym/lib/app/models/user_model.dart
@@ -1,32 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 //String name, String email, String password, String cpf, String login, UserRole role
 class UserModel {
   String name;
   String email;
-  String password;
   String cpf;
   String login;
   String role;
-  String? accessToken;
-
   UserModel({
     required this.name,
     required this.email,
-    required this.password,
     required this.cpf,
     required this.login,
     required this.role,
-    this.accessToken,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'name': name,
       'email': email,
-      'password': password,
       'cpf': cpf,
       'login': login,
       'role': role, // Convert enum to string
-      'accessToken': accessToken,
     };
   }
 
@@ -34,30 +28,30 @@ class UserModel {
     return UserModel(
       name: json['name'],
       email: json['email'],
-      password: json['password'],
       cpf: json['cpf'],
       login: json['login'],
       role: json['role'],
-      accessToken: json['accessToken'],
     );
   }
 
   UserModel copyWith({
     String? name,
     String? email,
-    String? password,
     String? cpf,
     String? login,
     String? role,
-    String? accessToken,
   }) {
     return UserModel(
       name: name ?? this.name,
       email: email ?? this.email,
-      password: password ?? this.password,
       cpf: cpf ?? this.cpf,
       login: login ?? this.login,
       role: role ?? this.role,
     );
+  }
+
+  @override
+  String toString() {
+    return 'UserModel(name: $name, email: $email, cpf: $cpf, login: $login, role: $role)';
   }
 }
