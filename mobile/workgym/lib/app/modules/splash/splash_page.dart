f1b1ec8c5b2f20829e4f3337c6e872deeaccfcf4
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-07 16:04:54 -0300

    feat: config splash page

---
+++ mobile/workgym/lib/app/modules/splash/splash_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:workgym/app/modules/auth/login/login_controller.dart';
+import 'package:workgym/app/utils/app_routes.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Evita executar notifyListeners durante o build
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _initApp();
+    });
+  }
+
+  Future<void> _initApp() async {
+    final controller = context.read<LoginController>();
+    final success = await controller.tryAutoLogin();
+
+    if (success) {
+      Navigator.pushReplacementNamed(context, AppRoutes.homePage);
+    } else {
+      Navigator.pushReplacementNamed(context, AppRoutes.loginPage);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
