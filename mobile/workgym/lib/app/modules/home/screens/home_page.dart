3baf30d3561f123bc73b881e03bd5f21fefd7144
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-08 10:38:43 -0300

    feat: update layout home page

--- mobile/workgym/lib/app/modules/home/screens/home_page.dart
+++ mobile/workgym/lib/app/modules/home/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:workgym/app/modules/auth/login/login_controller.dart';
 
@@ -10,67 +11,304 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  static const IconData alarm = IconData(
+  0xf4d5,
+  fontFamily: 'MaterialIcons',
+  fontPackage: 'flutter_material_icons',
+);
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<LoginController>(context).user;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Bem-vindo ao WorkGym, ${user?.name}!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/bg-home-page.png'),
+              fit: BoxFit.cover,
             ),
-            const Text(
-              'Você está na tela inicial.',
-              style: TextStyle(fontSize: 18),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            Text('Email: ${user?.email}', style: TextStyle(fontSize: 18)),
-            Text('Login: ${user?.login}', style: TextStyle(fontSize: 18)),
-            Text('CPF: ${user?.cpf}', style: TextStyle(fontSize: 18)),
-            Text('Role: ${user?.role}', style: TextStyle(fontSize: 18)),
-            const SizedBox(height: 20),
-            const SizedBox(height: 20),
-          ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              SizedBox(
+                //container para o logo e o texto de boas-vindas
+                child: Column(
+                  children: [
+                    Text(
+                      'Bem-vindo,',
+                      style: GoogleFonts.merriweather(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: .5,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '${user?.login}!' ?? 'Usuário não encontrado',
+                      style: GoogleFonts.merriweather(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: .5,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
+              //container com 2 cards
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withValues(alpha: 0.1),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(50),
+                      blurRadius: 10,
+                      offset: const Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 160,
+                          height: 160,
+                          child: Card(
+                            color: Colors.white.withAlpha(2),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.fitness_center,
+                                    size: 50,
+                                    color: Colors.white,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      'Meus Treinos',
+                                      style: GoogleFonts.merriweather(
+                                        textStyle: TextStyle(
+                                          color: Colors.white,
+                                          letterSpacing: .5,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 160,
+                          height: 160,
+                          child: Card(
+                            color: Colors.white.withAlpha(2),
+                            // width: 150,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.person,
+                                    size: 50,
+                                    color: Colors.white,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      'Meus Dados',
+                                      style: GoogleFonts.merriweather(
+                                        textStyle: TextStyle(
+                                          color: Colors.white,
+                                          letterSpacing: .5,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 160,
+                          height: 160,
+                          child: Card(
+                            color: Colors.white.withAlpha(2),
+                            // width: 150,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.punch_clock,
+                                    size: 50,
+                                    color: Colors.white,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      'Horários',
+                                      style: GoogleFonts.merriweather(
+                                        textStyle: TextStyle(
+                                          color: Colors.white,
+                                          letterSpacing: .5,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 160,
+                          height: 160,
+                          child: Card(
+                            color: Colors.white.withAlpha(2),
+                            // width: 150,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.business,
+                                    size: 50,
+                                    color: Colors.white,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      'Nossos Serviços',
+                                      style: GoogleFonts.merriweather(
+                                        textStyle: TextStyle(
+                                          color: Colors.white,
+                                          letterSpacing: .5,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
+              //container com imagem de fundo
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/logo-sem-bg.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(50),
+                      blurRadius: 10,
+                      offset: const Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () async {
-          final confirmLogout = await showDialog<bool>(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text('Confirmar Logout'),
-                content: const Text('Você tem certeza que deseja sair?'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(false),
-                    child: const Text('Cancelar'),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(true),
-                    child: const Text('Sair'),
-                  ),
-                ],
-              );
-            },
-          );
+      // floatingActionButton: FloatingActionButton.extended(
+      //   onPressed: () async {
+      //     final confirmLogout = await showDialog<bool>(
+      //       context: context,
+      //       builder: (BuildContext context) {
+      //         return AlertDialog(
+      //           title: const Text('Confirmar Logout'),
+      //           content: const Text('Você tem certeza que deseja sair?'),
+      //           actions: [
+      //             TextButton(
+      //               onPressed: () => Navigator.of(context).pop(false),
+      //               child: const Text('Cancelar'),
+      //             ),
+      //             TextButton(
+      //               onPressed: () => Navigator.of(context).pop(true),
+      //               child: const Text('Sair'),
+      //             ),
+      //           ],
+      //         );
+      //       },
+      //     );
 
-          if (confirmLogout == true) {
-            context.read<LoginController>().logout(context);
-            Navigator.pushReplacementNamed(context, '/login-page');
-          }
-        },
-        label: const Text('Logout', style: TextStyle(color: Colors.white)),
-        icon: const Icon(Icons.exit_to_app, color: Colors.white),
-        backgroundColor: Colors.redAccent,
-      ),
+      //     if (confirmLogout == true) {
+      //       context.read<LoginController>().logout(context);
+      //       Navigator.pushReplacementNamed(context, '/login-page');
+      //     }
+      //   },
+      //   label: Text(
+      //     'Logout',
+      //     style: GoogleFonts.merriweather(
+      //       textStyle: TextStyle(
+      //         color: Colors.black,
+      //         letterSpacing: .5,
+      //         fontSize: 20,
+      //       ),
+      //     ),
+      //   ),
+      //   icon: const Icon(Icons.exit_to_app, color: Colors.black),
+      //   backgroundColor: Colors.white,
+      // ),
     );
   }
 }
