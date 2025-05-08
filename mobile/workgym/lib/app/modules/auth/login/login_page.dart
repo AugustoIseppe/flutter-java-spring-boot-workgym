cc1e6a7c85291f3670bcf16f5051bcf1f40ad6c8
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-08 00:34:30 -0300

    refactor: changes button and inserted bg

--- mobile/workgym/lib/app/modules/auth/login/login_page.dart
+++ mobile/workgym/lib/app/modules/auth/login/login_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 // import 'package:provider/provider.dart';
 import 'package:validatorless/validatorless.dart';
 import 'package:workgym/app/modules/auth/login/login_controller.dart';
+import 'package:workgym/app/widgets/gradient_button.dart';
 import 'package:workgym/app/widgets/loader.dart';
 import 'package:workgym/app/widgets/todo_list_field.dart';
 
@@ -23,112 +24,113 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Consumer<LoginController>(
-          builder: (context, controller, child) {
-            return Stack(
-              children: [
-                Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        TodoListField(
-                          focusNode: _emailFocus,
-                          label: 'Login',
-                          controller: _emailEC,
-                          // validator: Validatorless.multiple([
-                          //   Validatorless.required('E=mail obrigatório'),
-                          //   Validatorless.email('Email inválido'),
-                          // ]),
-                        ),
-                        const SizedBox(height: 10),
-                        TodoListField(
-                          label: 'Senha',
-                          obscureText: true,
-                          controller: _passwordEC,
-                          validator: Validatorless.multiple([
-                            Validatorless.required('Senha obrigatória'),
-                            Validatorless.min(
-                              6,
-                              'A senha deve ter no mínimo 6 caracteres',
-                            ),
-                          ]),
-                        ),
-                        Consumer<LoginController>(
-                          builder: (context, controller, child) {
-                            return controller.isLoading
-                                ? const Center(
-                                  child: CircularProgressIndicator(),
-                                )
-                                : TextButton(
-                                  style: ButtonStyle(
-                                    backgroundColor: WidgetStateProperty.all(
-                                      Colors.blueAccent.shade700,
-                                    ),
-                                  ),
-                                  onPressed: () async {
-                                    final formValid =
-                                        _formKey.currentState!.validate();
-                                    if (formValid) {
-                                      final email = _emailEC.text;
-                                      final password = _passwordEC.text;
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/logo-principal.png"),
+              fit:
+                  BoxFit
+                      .cover, // ou .contain, .fill, etc. dependendo do seu layout
+            ),
+          ),
+          child: Consumer<LoginController>(
+            builder: (context, controller, child) {
+              return Stack(
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TodoListField(
+                            focusNode: _emailFocus,
+                            label: 'Login',
+                            controller: _emailEC,
+                            // validator: Validatorless.multiple([
+                            //   Validatorless.required('E=mail obrigatório'),
+                            //   Validatorless.email('Email inválido'),
+                            // ]),
+                          ),
+                          const SizedBox(height: 10),
+                          TodoListField(
+                            label: 'Senha',
+                            obscureText: true,
+                            controller: _passwordEC,
+                            validator: Validatorless.multiple([
+                              Validatorless.required('Senha obrigatória'),
+                              Validatorless.min(
+                                6,
+                                'A senha deve ter no mínimo 6 caracteres',
+                              ),
+                            ]),
+                          ),
+                          const SizedBox(height: 10),
+                          Consumer<LoginController>(
+                            builder: (context, controller, child) {
+                              return controller.isLoading
+                                  ? const Center(
+                                    child: CircularProgressIndicator(),
+                                  )
+                                  : GradientButton(
+                                    text: "Entrar",
 
-                                      final token = await context
-                                          .read<LoginController>()
-                                          .login(email, password);
+                                    onPressed: () async {
+                                      final formValid =
+                                          _formKey.currentState!.validate();
+                                      if (formValid) {
+                                        final email = _emailEC.text;
+                                        final password = _passwordEC.text;
 
-                                      if (token.isNotEmpty) {
-                                        // Aguarda o próximo frame para garantir que o estado foi atualizado
-                                        WidgetsBinding.instance
-                                            .addPostFrameCallback((_) {
-                                              Navigator.of(
-                                                context,
-                                              ).pushReplacementNamed(
-                                                '/home-page',
-                                              );
-                                            });
-                                      } else {
-                                        final error =
-                                            context
-                                                .read<LoginController>()
-                                                .error;
-                                        ScaffoldMessenger.of(
-                                          context,
-                                        ).showSnackBar(
-                                          SnackBar(
-                                            content: Text(
-                                              error ?? 'Erro ao fazer login',
+                                        final token = await context
+                                            .read<LoginController>()
+                                            .login(email, password);
+
+                                        if (token.isNotEmpty) {
+                                          // Aguarda o próximo frame para garantir que o estado foi atualizado
+                                          WidgetsBinding.instance
+                                              .addPostFrameCallback((_) {
+                                                Navigator.of(
+                                                  context,
+                                                ).pushReplacementNamed(
+                                                  '/home-page',
+                                                );
+                                              });
+                                        } else {
+                                          final error =
+                                              context
+                                                  .read<LoginController>()
+                                                  .error;
+                                          ScaffoldMessenger.of(
+                                            context,
+                                          ).showSnackBar(
+                                            SnackBar(
+                                              content: Text(
+                                                error ?? 'Erro ao fazer login',
+                                              ),
                                             ),
-                                          ),
-                                        );
+                                          );
+                                        }
                                       }
-                                    }
-                                  },
-
-                                  child: const Text(
-                                    'Login',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                );
-                          },
-                        ),
-                      ],
+                                    },
+                                  );
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Loader(
-                  show: controller.isLoading,
-                  message: 'Realizando login, por favor aguarde...',
-                ),
-              ],
-            );
-          },
+                  Loader(
+                    show: controller.isLoading,
+                    message: 'Realizando login, por favor aguarde...',
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
