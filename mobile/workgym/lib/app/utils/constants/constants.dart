d36097f6c0fdaa7958636ed50c9782e5070fa5c2
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-08 16:46:50 -0300

    chore: weekday url

--- mobile/workgym/lib/app/utils/constants/constants.dart
+++ mobile/workgym/lib/app/utils/constants/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
   final String loginUrl = 'http://192.168.1.100:8080/auth/login';
   final String getMeUrl = 'http://192.168.1.100:8080/auth/me';
+  final String getWeekDayUrl = 'http://192.168.1.100:8080/user-exercises';
 }
