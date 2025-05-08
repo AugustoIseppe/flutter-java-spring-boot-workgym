633610d4e6fc703071a7f6390c2a3f0bd52b8a4f
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-08 16:47:15 -0300

    chore: weekday page routes

--- mobile/workgym/lib/app/utils/app_routes.dart
+++ mobile/workgym/lib/app/utils/app_routes.dart
@@ -2,4 +2,6 @@ class AppRoutes {
   static const splash = "/"; // Agora a Splash será a rota inicial
   static const loginPage = "/login-page";
   static const homePage = "/home-page";
+  static const weekDay = "/week-day";
+  static const weekDayDetail = "/week-day-detail";
 }
