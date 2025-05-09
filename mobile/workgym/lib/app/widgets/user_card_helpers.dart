0ac1503fe62e518bc3d77f20f5b1114aaa863946
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-09 11:25:27 -0300

    chore: update layout

--- mobile/workgym/lib/app/widgets/user_card_helpers.dart
+++ mobile/workgym/lib/app/widgets/user_card_helpers.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Widget buildUserCard(String label, String value) {
-  return Card(
-    color: Colors.white.withValues(alpha: .85),
-    margin: const EdgeInsets.symmetric(vertical: 8),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+  return Container(
+    margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    width: double.infinity,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      border: Border.all(color: Colors.white, width: 1),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withAlpha(100),
+          blurRadius: 10,
+          offset: const Offset(0, 0),
+        ),
+      ],
+    ),
     child: ListTile(
-      title: Text(
-        label,
-        style: const TextStyle(fontWeight: FontWeight.bold),
-      ),
-      subtitle: Text(value),
-      leading: const Icon(Icons.person_outline),
+      title: Text(label, style: GoogleFonts.merriweather(
+        textStyle: const TextStyle(
+          color: Colors.white,
+          letterSpacing: .5,
+          fontSize: 16,
+        ),
+      )),
+      subtitle: Text(value , style: GoogleFonts.merriweather(
+        textStyle: const TextStyle(
+          color: Colors.white,
+          letterSpacing: .5,
+          fontSize: 14,
+        ),
+      )),
+      leading: const Icon(Icons.person_outline, color: Colors.white),
     ),
   );
 }
