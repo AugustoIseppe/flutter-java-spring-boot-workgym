8ce47e65100889b421e3321fba03de875f55f8f7
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-12 17:20:32 -0300

    chore: inserted logo

--- web/workgym/src/app/(protected)/dashboard/page.tsx
+++ web/workgym/src/app/(protected)/dashboard/page.tsx
@@ -1,15 +1,15 @@
-// src/app/(protected)/dashboard/page.tsx
 "use client";
-import { useAuth } from "@/app/authContext";
 import Pagina from "@/components/template/Pagina";
 import Image from "next/image";
-// import { useAuth } from '@/contexts/AuthContext';
+
+import { useAuth } from "@/app/authContext";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
 export default function DashboardPage() {
   const { isAuthenticated, isLoading } = useAuth();
-
+  const { token } = useAuth();
+ 
   useEffect(() => {
     if (!isLoading && !isAuthenticated) {
       redirect("/login");
@@ -26,7 +26,14 @@ export default function DashboardPage() {
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-4 text-zinc-950">Dashboard</h1>
           <p className="text-lg text-zinc-950">Bem-vindo ao seu painel!</p>
-          <Image src={"/workgym-logo.png"} alt="Explorador" width={200} height={200} className="mt-4" />
+          <Image
+            src={"/workgym-logo.png"}
+            alt="Explorador"
+            width={200}
+            height={200}
+            className="mt-4"
+          />
+          
         </div>
       </div>
     </Pagina>
