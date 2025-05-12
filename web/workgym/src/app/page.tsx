d464a67dd218f0691ae4881431988a8b85ccbae6
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-12 11:16:30 -0300

    feat: update home page dashboard

--- web/workgym/src/app/page.tsx
+++ web/workgym/src/app/page.tsx
@@ -1,19 +1,5 @@
-import Image from "next/image";
-import Pagina from "@/components/template/Pagina";
+import { redirect } from 'next/navigation'
 
-export default function Home() {
-  return (
-    <Pagina>
-      <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="text-4xl font-bold mb-4 text-cyan-950">Bem-vindo ao WorkGym!</h1>
-        <Image
-          src="/workgym-logo.png"
-          alt="Logo"
-          width={200}
-          height={200}
-          className="logo-clickhub-homepage elevation-3"
-        />
-      </div>
-    </Pagina>
-  );
+export default async function Home() { // Note o async aqui
+  
 }
