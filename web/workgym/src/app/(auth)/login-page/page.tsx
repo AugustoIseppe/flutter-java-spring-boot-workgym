12c5f8c40f426c67e50b54c15d7a6e576f136496
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-12 18:09:26 -0300

    chore: adjustment in indentation and blank lines

--- web/workgym/src/app/(auth)/login-page/page.tsx
+++ web/workgym/src/app/(auth)/login-page/page.tsx
@@ -30,64 +30,67 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-row bg-white mx-auto w-full h-screen justify-around items-center "> 
+    <div className="flex flex-row bg-white mx-auto w-full h-screen justify-around items-center ">
       <div className="flex flex-col items-center justify-center min-h-screen flex-1">
         <Image
-                    src={"/WORK.png"}
-                    alt="Explorador"
-                    width={400}
-                    height={400}
-                    className=""
-                  />
-                  <h1 className="text-4xl font-bold mb-4 text-zinc-950">Bem-vindo ao WorkGym</h1>
-        <p className="text-lg text-zinc-950">Sua jornada de trabalho começa aqui!</p>
+          src={"/WORK.png"}
+          alt="Explorador"
+          width={400}
+          height={400}
+          className=""
+        />
+        <h1 className="text-4xl font-bold mb-4 text-zinc-950">
+          Bem-vindo ao WorkGym
+        </h1>
+        <p className="text-lg text-zinc-950">
+          Sua jornada de trabalho começa aqui!
+        </p>
       </div>
 
       {/* Formulário de Login */}
-    <div className="flex flex-col items-center justify-center min-h-screen flex-1 bg-gray-100">
-      <div className="bg-white p-6 rounded-2xl shadow-md w-96 flex flex-col items-center text-center ">
-        
-                <h1 className="text-zinc-950 text-5xl mb-4">Login</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen flex-1 bg-gray-100">
+        <div className="bg-white p-6 rounded-2xl shadow-md w-96 flex flex-col items-center text-center ">
+          <h1 className="text-zinc-950 text-5xl mb-4">Login</h1>
 
-        <form onSubmit={handleSubmit} className="w-full">
-          <div className="mb-4 text-left">
-            <label htmlFor="login" className="text-zinc-950">
-              Login:
-            </label>
-            <input
-              type="text"
-              id="login"
-              value={loginValue}
-              onChange={(e) => setLoginValue(e.target.value)}
-              required
-              className="border p-2 rounded text-black w-full"
-            />
-          </div>
-          <div className="mb-4 text-left">
-            <label htmlFor="password" className="text-zinc-950">
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="border p-2 rounded text-black w-full"
-            />
-          </div>
-          {error && <p className="text-red-500 mb-2">{error}</p>}
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50 w-full"
-          >
-            {isLoading ? "Logging in..." : "Login"}
-          </button>
-        </form>
+          <form onSubmit={handleSubmit} className="w-full">
+            <div className="mb-4 text-left">
+              <label htmlFor="login" className="text-zinc-950">
+                Login:
+              </label>
+              <input
+                type="text"
+                id="login"
+                value={loginValue}
+                onChange={(e) => setLoginValue(e.target.value)}
+                required
+                className="border p-2 rounded text-black w-full"
+              />
+            </div>
+            <div className="mb-4 text-left">
+              <label htmlFor="password" className="text-zinc-950">
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="border p-2 rounded text-black w-full"
+              />
+            </div>
+            {error && <p className="text-red-500 mb-2">{error}</p>}
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 disabled:opacity-50 w-full"
+            >
+              {isLoading ? "Logging in..." : "Login"}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
