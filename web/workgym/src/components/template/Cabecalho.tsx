914a099729c62a60e84149c6381e4e48a27a66ca
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-12 17:20:57 -0300

    feat: update header

--- web/workgym/src/components/template/Cabecalho.tsx
+++ web/workgym/src/components/template/Cabecalho.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { IconLogout } from "@tabler/icons-react";
 import React, { useContext } from "react";
 import AuthContext from "@/app/authContext";
@@ -6,6 +8,7 @@ import Image from "next/image";
 export default function Cabecalho() {
   const auth = useContext(AuthContext); // ✅ dentro do componente
   const logout = auth?.logout;
+
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -19,21 +22,25 @@ export default function Cabecalho() {
   return (
     <header className="flex justify-between items-center bg-white border-b px-6 py-3 gap-2 ">
       <div className="flex items-center justify-center w-12 h-12 bg-black rounded-full">
-
         <Image src={"/workgym-logo.png"} alt="Logo" width={40} height={40} />
       </div>
-        <span className="font-bold text-zinc-950 text-lg">
-          WorkGym - Portal de Gerenciamento da Academia
-        </span>
+      <span className="font-bold text-zinc-950 text-lg">
+        WorkGym - Portal de Gerenciamento da Academia
+      </span>
 
+      <div className="flex items-center gap-2">
+        <span className="text-zinc-950 text-sm">
+          {auth?.user?.login} - {auth?.user?.role} 
+        </span>
       <form onSubmit={handleSubmit}>
         <button
           type="submit"
           className=" text-white px-4 py-1 rounded hover:bg-cyan-800 transition duration-200"
-        >
-            <IconLogout size={30} stroke={1.5}  color="black"/>
+          >
+          <IconLogout size={30} stroke={1.5} color="black" />
         </button>
       </form>
+          </div>
     </header>
   );
 }
