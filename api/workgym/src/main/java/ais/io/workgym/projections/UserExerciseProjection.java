cb8df294cd7226aeef2c15f29b18549e140b9c56
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-13 19:36:22 -0300

    feat: user exercise projection

---
+++ api/workgym/src/main/java/ais/io/workgym/projections/UserExerciseProjection.java
@@ -0,0 +1,13 @@
+package ais.io.workgym.projections;
+
+public interface UserExerciseProjection {
+    String getName();
+    String getDescription();
+    String getMuscleGroup();
+    String getImage();
+    String getEquipment();
+    Integer getSeries();
+    String getWeekDay();
+    Integer getRepetitions();
+    String getObservation();
+}
