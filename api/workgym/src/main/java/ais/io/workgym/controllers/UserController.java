f08c284fa21fc253e89bd3d42756453f84c4006f
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-13 19:33:38 -0300

    feat: reset password

--- api/workgym/src/main/java/ais/io/workgym/controllers/UserController.java
+++ api/workgym/src/main/java/ais/io/workgym/controllers/UserController.java
@@ -1,17 +1,24 @@
 package ais.io.workgym.controllers;
 
+import ais.io.workgym.dto.PasswordResetDTO;
+import ais.io.workgym.dto.user.ChangePasswordDTO;
 import ais.io.workgym.dto.user.UserRequestDTO;
 import ais.io.workgym.dto.user.UserResponseDTO;
+import ais.io.workgym.entities.User;
+import ais.io.workgym.repositories.UserRepository;
 import ais.io.workgym.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -20,6 +27,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
 //    @PostMapping
 //    public ResponseEntity<UserResponseDTO> insert(@Valid @RequestBody UserRequestDTO userRequestDTO) {
@@ -55,4 +65,20 @@ public class UserController {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}/reset-password")
+    @PreAuthorize("hasRole('ADMIN')") // só admins podem trocar senhas de outros usuários
+    public ResponseEntity<?> changePassword(@PathVariable UUID id, @RequestBody PasswordResetDTO dto) {
+        try {
+            userService.changePassword(id, dto.newPassword());
+            return ResponseEntity.ok("Senha atualizada com sucesso");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao atualizar senha: " + e.getMessage());
+        }
+    }
+
+
+
+
+
 }
