1b9b1fb8857461c55e181485e174b438b05976da
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-05-13 19:33:20 -0300

    refactor: changed to returns user response

--- api/workgym/src/main/java/ais/io/workgym/controllers/AuthenticationController.java
+++ api/workgym/src/main/java/ais/io/workgym/controllers/AuthenticationController.java
@@ -3,7 +3,9 @@ package ais.io.workgym.controllers;
 import ais.io.workgym.dto.AuthenticationDTO;
 import ais.io.workgym.dto.LoginResponseDTO;
 import ais.io.workgym.dto.RegisterDTO;
+import ais.io.workgym.dto.user.ChangePasswordDTO;
 import ais.io.workgym.dto.user.UserDTO;
+import ais.io.workgym.dto.user.UserRegisterResponseDTO;
 import ais.io.workgym.entities.User;
 import ais.io.workgym.infra.security.TokenService;
 import ais.io.workgym.repositories.UserRepository;
@@ -16,6 +18,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -44,11 +48,32 @@ public class AuthenticationController {
         }
     }
 
-    @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
+    // @PostMapping("/register")
+    // public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
+    //
+    // if (userRepository.findByLogin(data.login()) != null) {
+    // return ResponseEntity.status(400).body("User already exists");
+    // }
+    //
+    // String encryptedPassword = new
+    // BCryptPasswordEncoder().encode(data.password());
+    // User user = new User(
+    // data.name(),
+    // data.email(),
+    // encryptedPassword,
+    // data.cpf(),
+    // data.login(),
+    // data.role());
+    // userRepository.save(user);
+    // return ResponseEntity
+    // .status(201) // 201 CREATED é mais apropriado para criação de recursos
+    // .body(user);
+    // }
 
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterDTO data) {
         if (userRepository.findByLogin(data.login()) != null) {
-            return ResponseEntity.status(400).body("User already exists");
+            return ResponseEntity.status(400).body(Map.of("error", "User already exists"));
         }
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
@@ -59,8 +84,11 @@ public class AuthenticationController {
                 data.cpf(),
                 data.login(),
                 data.role());
+
         userRepository.save(user);
-        return ResponseEntity.ok("User registered successfully");
+
+        // Retorne o usuário criado como JSON (sem senha)
+        return ResponseEntity.status(201).body(new UserRegisterResponseDTO(user));
     }
 
     @GetMapping("/me")
@@ -74,6 +102,23 @@ public class AuthenticationController {
 
         User user = userRepository.findByLogin(login);
         return ResponseEntity.ok(new UserDTO(user));
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordDTO data) {
+        User user = userRepository.findByLogin(data.login());
+        if (user == null) {
+            return ResponseEntity.status(404).body("Usuário não encontrado");
+        }
+
+        if (!new BCryptPasswordEncoder().matches(data.oldPassword(), user.getPassword())) {
+            return ResponseEntity.status(400).body("Senha atual incorreta");
+        }
+
+        user.setPassword(new BCryptPasswordEncoder().encode(data.newPassword()));
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Senha atualizada com sucesso");
     }
 
 }
